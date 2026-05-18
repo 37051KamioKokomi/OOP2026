11d3fe41611ea4096db516e33258d5e0631610c6
神尾 心美 <oibc37051@ojs.ac.jp>
2026-05-18 11:43:49 +0900

    ラムダ式とはp.68

--- Chapter03/ラムダ式とLINQ/Section01/Program.cs
+++ Chapter03/ラムダ式とLINQ/Section01/Program.cs
@@ -1,16 +1,20 @@
 ﻿namespace Section01 {
     internal class Program {
+
+        
+
         static void Main(string[] args) {
-            var count = Count(5);
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+
+            var count = Count(numbers, n => n % 4 == 0 || n % 5 == 0);//delegate(int n){ return n % 2 == 0; });//処理の異常
             Console.WriteLine(count);
         }
 
-
-        static int Count(int num) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+        static int Count(int[] numbers,Predicate<int> judge) {//山かっこ、型引数
+            
             var count = 0;
             foreach (var n in numbers) {
-                if (n == num) {
+                if (judge(n) == true) {
                     count++;
                 }
             }
