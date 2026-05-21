421c2a7670c6e64898f7360855282b50086139c0
神尾 心美 <oibc37051@ojs.ac.jp>
2026-05-21 09:55:10 +0900

    問題3.1.1完成

--- Chapter03/ラムダ式とLINQ/Section01/Program.cs
+++ Chapter03/ラムダ式とLINQ/Section01/Program.cs
@@ -1,24 +1,27 @@
 ﻿namespace Section01 {
     internal class Program {
 
-        
-
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var cities = new List<string> {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
 
-            var count = Count(numbers, n => n % 4 == 0 || n % 5 == 0);//delegate(int n){ return n % 2 == 0; });//処理の異常
-            Console.WriteLine(count);
-        }
-
-        static int Count(int[] numbers,Predicate<int> judge) {//山かっこ、型引数
-            
-            var count = 0;
-            foreach (var n in numbers) {
-                if (judge(n) == true) {
-                    count++;
-                }
-            }
-            return count;
+            //以下の条件を満たすものが存在するか調べる
+            //【教科書P134～137を参考に】
+            //・文字数が6文字以上
+            //・o を含む
+            //・最後が n
+            var exists = cities.Exists(s => 6 <= s.Length && s.Contains('o') && s.EndsWith('n'));
+            Console.WriteLine(exists);
         }
     }
 }
+
+
---
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -0,0 +1,50 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+            // 3.1.1
+            Console.WriteLine("--- 3.1.1 ---");
+            Exercise1(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.2
+            Console.WriteLine("--- 3.1.2 ---");
+            Exercise2(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.3
+            Console.WriteLine("--- 3.1.3 ---");
+            Exercise3(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.4
+            Console.WriteLine("--- 3.1.4 ---");
+            Exercise4(numbers);
+        }
+
+        private static void Exercise1(List<int> numbers) {
+            //できたらGitのコメント「問題3.1.1完成」
+            var exists = numbers.Exists(n => n % 8 == 0 || n % 9 ==0);
+            if (exists) {
+                Console.WriteLine("存在しています");
+            }else {
+                Console.WriteLine("存在していません");
+            }
+        }
+
+        private static void Exercise2(List<int> numbers) {
+            //できたらGitのコメント「問題3.1.2完成」
+
+        }
+
+        private static void Exercise3(List<int> numbers) {
+            //できたらGitのコメント「問題3.1.3完成」
+        }
+
+        private static void Exercise4(List<int> numbers) {
+            //できたらGitのコメント「問題3.1.4完成」
+        }
+    }
+}
