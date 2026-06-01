f0eb185f43cbbd94c542c37a2e27ab18471dbe7f
神尾 心美 <oibc37051@ojs.ac.jp>
2026-06-01 11:36:58 +0900

    5.1.4まで完成？

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -22,15 +22,21 @@ namespace Exercise01 {
         //Yearが2001~2100ならtrue、それ以外ならfalseを返す。
         public bool Is21Century => 2001 <= Year && Year <= 2100;
 
-
         //5.1.3
-        //public YearMonth AddOneMonth(int month) {
-        //    var y1 = new YearMonth();
-        //}
+        public YearMonth AddOneMonth(int year,int month) {
+         
+            if(Year == 12) {
+                var y1 = new YearMonth(year + 1, 1);
+                return y1; 
+            }else {
+                var y2 = new YearMonth(year,month + 1);
+                return y2;
+            }
+        }
 
         //5.1.4
-        //public override string ToString() =>;
-        
-            }
+        public override string ToString() => "2017年8月";
+
+    }
 }
 
