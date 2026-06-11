4db6ef093f45feeb0fdd28e4cb349b4a92779914
神尾 心美 <oibc37051@ojs.ac.jp>
2026-06-11 11:03:52 +0900

    辞書(ディクショナリ)を使った集計　その二

--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -61,19 +61,15 @@ namespace Exercise03 {
             var str = text.ToLower().Replace(" ","");
 
             //辞書(ディクショナリ)を使う方法
-            //var alphaDicCount = Enumerable.Range('a',26).ToDictionary(num => ((char)num).ToString(),num => 0);
+            var alphaDicCount = Enumerable.Range('a',26).ToDictionary(num => ((char)num),num => 0);
             //foreach(var alpha in str.Where(s => s.Contains(alphaDicCount)) {
 
             //}
-            var dict = new SortedDictionary<char, int>();
+            //var dict = new SortedDictionary<char, int>();
             foreach (var c in str) {
-                if (dict.ContainsKey(c)) {
-                    dict[c]++; //登録されている場合
-                } else {
-                    dict[c] = 1; //未登録の場合
-                }
+                alphaDicCount[c]++;
             }
-            foreach (var word in dict) {
+            foreach (var word in alphaDicCount) {
                 Console.WriteLine(word.Key + ":" + word.Value);
             }
 
