3d6b63310472f57098b232951ea8a727e3610e02
神尾 心美 <oibc37051@ojs.ac.jp>
2026-06-11 10:19:00 +0900

    問題6.3.5まで

---
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Jackdaws love my big sphinx of quartz";
+            #region
+            Console.WriteLine("6.3.1");
+            Exercise1(text);
+
+            Console.WriteLine("6.3.2");
+            Exercise2(text);
+
+            Console.WriteLine("6.3.3");
+            Exercise3(text);
+
+            Console.WriteLine("6.3.4");
+            Exercise4(text);
+
+            Console.WriteLine("6.3.5");
+            Exercise5(text);
+
+            Console.WriteLine("6.3.99");
+            Exercise6(text);
+            #endregion
+        }
+
+        private static void Exercise1(string text) {
+            var spaces = text.Count(c => c == ' ');
+            Console.WriteLine($"空白数:{spaces}");
+            //別の書き方
+            //Console.WriteLine("空白数:{0}", spaces);
+        }
+
+        private static void Exercise2(string text) {
+            Console.Write("検索：");
+            var search = Console.ReadLine();
+            Console.Write("置換：");
+            var replace = Console.ReadLine();
+
+            var replaced = text.Replace(search, replace);
+            Console.WriteLine(replaced);
+        }
+
+        private static void Exercise3(string text) {
+            //やらなくてよい
+        }
+
+        private static void Exercise4(string text) {
+            var count = text.Split(' ').Length;
+            Console.WriteLine($"単語数:{count}");
+        }
+
+        private static void Exercise5(string text) {
+            text.Split(' ').Where(s => s.Length <= 4).ToList().ForEach(Console.WriteLine); 
+            //即時実行でリスト指定ならToList、配列指定ならToArray。
+        }
+
+        //アルファベットの数をカウントして表示する
+        private static void Exercise6(string text) {
+
+            
+            //辞書(ディクショナリ)を使う方法
+
+            //配列を用いた集計
+
+            //'a'から順にカウントして集計
+
+            //var arpha = text.
+            //for(){
+
+            //}
+        }
+    }
+}
