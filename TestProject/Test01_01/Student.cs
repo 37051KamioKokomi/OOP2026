35d32e25ba5823c9d12b65f827bbb749455dc55c
神尾 心美 <oibc37051@ojs.ac.jp>
2026-06-22 11:35:09 +0900

    設問1_Studentクラス完成

--- TestProject/Test01_01/Student.cs
+++ TestProject/Test01_01/Student.cs
@@ -1,7 +1,7 @@
 ﻿namespace Test01_01 {
     public class Student {
-        
-
-
+        public string Name { get; init; } = null!; //学生の名前
+        public string Subject { get; init; } = null!; //科目名
+        public int Score { get; init; } = 0; //点数
     }
 }
