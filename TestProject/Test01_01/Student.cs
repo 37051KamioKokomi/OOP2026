de50c0c49c566c106132417168b72344a3fc9159
神尾 心美 <oibc37051@ojs.ac.jp>
2026-06-22 12:05:11 +0900

    テスト終了

--- TestProject/Test01_01/Student.cs
+++ TestProject/Test01_01/Student.cs
@@ -2,10 +2,10 @@
     //学生クラス
     public class Student {
         //学生の名前
-        public string Name { get; init; } = string.Empty;
+        public string Name { get; private set; } = string.Empty;
         //科目名
-        public string Subject { get; init; } = string.Empty;
+        public string Subject { get; private set; } = string.Empty;
         //点数
-        public int Score { get; init; } = 0; 
+        public int Score { get; private set; } = 0; 
     }
 }
--- TestProject/Test01_02/Program.cs
+++ TestProject/Test01_02/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Test01_02 {
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Test01_02 {
     internal class Program {
         static void Main(string[] args) {
             var numbers = new[] { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
@@ -61,40 +64,45 @@
         //問題１　合計値を表示（式形式で記述せよ）
         //　　　　出力結果【618】
         private static void Exercise01(int[] numbers) {
-
+            Console.WriteLine(numbers.Sum());
         }
 
 
         //問題２　偶数の最大値を表示（式形式で記述せよ）
         //　　　　出力結果【94】
         private static void Exercise02(int[] numbers) {
-
+            Console.WriteLine(numbers.Where(n => n % 2 == 0).Max());
         }
 
 
         //問題３　昇順に並べて表示（遅延実行とする）
         //　　　　出力結果【12 14 17 20 31 35 40 48 53 76 87 91 94】
         private static void Exercise03(int[] numbers) {
-            
+            var num = numbers.Order();
+            foreach(var n in num) {
+                Console.Write(n + " ");
+            }
         }
 
         //問題４　10以上50以下の数字のみを表示（即時実行でも可とする）
         //　　　　出力結果【12 14 20 40 35 31 17 48】
         private static void Exercise04(int[] numbers) {
-            
+            var numm = numbers.Where(n => n >= 10 && n <= 50);
+            foreach (var n in numm) {
+                Console.Write(n + " ");
+            }
         }
 
         //問題５　Countメソッドを使い、小文字の'n'が含まれている都市名がいくつあるかカウントして結果を表示
         //　　　　出力結果【5】
         private static void Exercise05(List<string> cities) {
-      
-        
+            Console.Write(cities.Count(n => n.Contains('n')));
         }
 
         //問題６　全都市数
         //　　　　出力結果【8】
         private static void Exercise06(List<string> cities) {
-        
+            Console.Write(cities.Count());
         }
 
         //問題７　各都市名をアルファベット順（昇順）に出力
@@ -108,7 +116,7 @@
         //          Paris
         //          Tokyo】
         private static void Exercise07(List<string> cities) {
-        
+            
 
         }
 
