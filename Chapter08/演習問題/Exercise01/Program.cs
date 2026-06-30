b49465155987363816c389343da4ebea0548afda
神尾 心美 <oibc37051@ojs.ac.jp>
2026-06-30 15:14:31 +0900

    8.1.1わからない

--- Chapter08/演習問題/Exercise01/Program.cs
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -1,6 +1,10 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 namespace Exercise01 {
     internal class Program {
+
+
         static void Main(string[] args) {
             var text = "Cozy lummux gives smart squid who asks for job pen";
 
@@ -11,13 +15,37 @@ namespace Exercise01 {
         }
 
         private static void Exercise1(string text) {
-            //if('A'<= char && char <= 'Z') {
+            //コミットのコメント(問題8.1.1完成)
+            var dict = new Dictionary<char,int>();
 
-            //}
+            
+            foreach (var t in text) {
+                //辞書のキーに登録されているか ?
+                if (dict.ContainsKey(t))
+                    //登録されている場合
+                    dict[t] += dict[t]; //売り上げを足しこみ
+                Console.WriteLine(dict[t]);
+                //else
+                //    //未登録の場合
+                //    dict[sale.ShopName] = sale.Amount; //新規に売り上げを登録
+            }
+            //// 要素を追加
+            // public void Add(string abbr, string japanese) => ;
+
+            
+
+            char ch = 'A';
+
+            if ('A' <= ch && ch <= 'Z') {
+
+            }
         }
 
+        
+
         private static void Exercise2(string text) {
-            
+            //コミットのコメント(問題8.1.1完成)
+
         }
 
     }
