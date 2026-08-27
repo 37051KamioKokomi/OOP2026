31fec85e96ac27ee9c8eeb2f65f0d666458ff804
神尾 心美 <oibc37051@ojs.ac.jp>
2026-08-27 14:05:00 +0900

    コメント追加

--- FormApps/CarReportSystem/Settings.cs
+++ FormApps/CarReportSystem/Settings.cs
@@ -21,7 +21,7 @@ namespace CarReportSystem {
         //外部からnewできないようにする
         private Settings() { }
 
-
+        //設定ファイルからロード
         public void Load() {
             if (!File.Exists(FileName)) {
                 return;
@@ -35,6 +35,7 @@ namespace CarReportSystem {
             }
         }
 
+        //設定ファイルを保存
         public void Save() {
             var data = new SettingsData {
                 MainFormBackColor = MainFormBackColor
