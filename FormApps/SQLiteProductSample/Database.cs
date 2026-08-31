cc430c445f7662b256e2623dcc1859c782dbe091
神尾 心美 <oibc37051@ojs.ac.jp>
2026-08-31 15:02:26 +0900

    Databaseクラス作成テストOK

--- FormApps/SQLiteProductSample/Database.cs
+++ FormApps/SQLiteProductSample/Database.cs
@@ -11,7 +11,7 @@ public static class Database {
 
     //SQLiteへ接続するための文字列
     private static readonly string ConnectionString =
-        $"Data Sorce={DatabasesPath}";
+        $"Data Source={DatabasesPath}";
 
     //DBファイルの保存場所を外部から確認するための読み取り専用プロパティ
     public static string FilePath => DatabasesPath;
