02d3b33edc7b363de805d810339cd2771d5140d6
心美 神尾 <oibc37051@ojs.ac.jp>
2026-09-08 13:16:41 +0900

    更新

--- TestProject/Test01_01/Student.cs
+++ TestProject/Test01_01/Student.cs
@@ -2,10 +2,10 @@
     //学生クラス
     public class Student {
         //学生の名前
-        public required string Name { get; init; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
         //科目名
-        public required string Subject { get; init; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
         //点数
-        public required int Score { get; init; } = 0; 
+        public int Score { get;  set; } = 0; 
     }
 }
